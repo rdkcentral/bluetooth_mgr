30c15bbd88ac3127be43713d4dc90aaccb51be06
nmuthu523 <Natraj_Muthusamy@comcast.com>
2025-04-01 08:25:30 +0000

    Revert "Revert "DELIA-67586 : Elite controller not disconnecting on deepsleep.""
    This reverts commit f591a3b2ad794f0fcd84a214ad6b34a917563a89.

--- src/ifce/btrMgr.c
+++ src/ifce/btrMgr.c
@@ -179,6 +179,7 @@ STATIC BTRMgrDeviceHandle               ghBTRMgrDevHdlConnInProgress = 0;
 #ifdef RDKTV_PERSIST_VOLUME
 STATIC BTRMgrDeviceHandle               ghBTRMgrDevHdlVolSetupInProgress = 0;
 #endif
+static gboolean                         isDeinitInProgress = FALSE;
 
 STATIC BTRMGR_DiscoveryHandle_t         ghBTRMgrDiscoveryHdl;
 STATIC BTRMGR_DiscoveryHandle_t         ghBTRMgrBgDiscoveryHdl;
@@ -3751,6 +3752,7 @@ BTRMGR_Init (
     GThread*        pMainLoopThread= NULL;
 
     char            lpcBtVersion[BTRCORE_STR_LEN] = {'\0'};
+    isDeinitInProgress = FALSE;
 
     if (ghBTRCoreHdl) {
         BTRMGRLOG_WARN("Already Inited; Return Success\n");
@@ -3926,7 +3928,7 @@ BTRMGR_DeInit (
     unsigned short                  ui16LoopIdx       = 0;
     BTRMGR_ConnectedDevicesList_t   lstConnectedDevices;
     unsigned int                    ui32sleepTimeOut = 1;
-
+    isDeinitInProgress = TRUE;
 
     if (btrMgr_isTimeOutSet()) {
         btrMgr_ClearDiscoveryHoldOffTimer();
@@ -10116,7 +10118,8 @@ btrMgr_ConnectionInAuthenticationCb (
             ((BTRMGR_XBOX_ELITE_VENDOR_ID == MVendorId && BTRMGR_XBOX_ELITE_PRODUCT_ID == ui32MProductId) ||
             (BTRMGR_XBOX_GAMESIR_VENDOR_ID == MVendorId && BTRMGR_XBOX_GAMESIR_PRODUCT_ID == ui32MProductId) ||
             (BTRMGR_NINTENDO_GAMESIR_VENDOR_ID == MVendorId && BTRMGR_NINTENDO_GAMESIR_PRODUCT_ID == ui32MProductId) ||
-            (BTRMGR_XBOX_ADAPTIVE_VENDOR_ID == MVendorId && BTRMGR_XBOX_ADAPTIVE_PRODUCT_ID == ui32MProductId)))) {
+            (BTRMGR_XBOX_ADAPTIVE_VENDOR_ID == MVendorId && BTRMGR_XBOX_ADAPTIVE_PRODUCT_ID == ui32MProductId))) &&
+            isDeinitInProgress != TRUE) {
 
             if (apstConnCbInfo->stKnownDevice.tDeviceId != ghBTRMgrDevHdlPairingInProgress &&
                 apstConnCbInfo->stKnownDevice.tDeviceId != ghBTRMgrDevHdlConnInProgress) {
