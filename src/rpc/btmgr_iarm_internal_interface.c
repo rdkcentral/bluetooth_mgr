a1b3df12cdda8c68c42f4cb30ab200c98ece1190
ppalan289 <preethi_palanisamy@comcast.com>
2025-04-16 21:30:24 +0530

    RDKEMW-3121:Update BTmgr client to use Power manager Plugin
    Reason for change: Replacing IARM PowerManager calls with PowerManager Plugin calls.
    Test Procedure: debug purpose 
    Risks: Low 
    Priority: P2

--- src/rpc/btmgr_iarm_internal_interface.c
+++ src/rpc/btmgr_iarm_internal_interface.c
@@ -2233,8 +2233,8 @@ BTRMgr_TermIARMMode (
         BTRMGRLOG_INFO ("IARM Interface Being terminated\n");
         IARM_Bus_Disconnect();
         IARM_Bus_Term();
-	BTRMGRLOG_ERROR ("Preethi: PoweController_Term started in internal\n");
-	PowerController_Term()
+	BTRMGRLOG_ERROR ("Preethi: PoweController_Term started in internal\n")
+	PowerController_Term();
 	BTRMGRLOG_ERROR ("Preethi: PoweController_Term ended in internal\n");
     }
     else {
