0ffd688015ba83e22b3fdce2e0c983ebc8940f19
ANANTHMARIMUTHU <49443026+ANANTHMARIMUTHU@users.noreply.github.com>
2025-08-06 15:59:39 -0400

    Update test_btrMgr.c

--- unitTest/unitTest_btmgr/test_btrMgr.c
+++ unitTest/unitTest_btmgr/test_btrMgr.c
@@ -5608,8 +5608,7 @@ void test_BTRMGR_Init_Success(void)
     // Assert the expected result
     BTRMgr_PI_Init_IgnoreAndReturn(eBTRMgrSuccess);
     BTRMgr_SD_Init_IgnoreAndReturn(eBTRMgrFailure);
-    BTRCore_refreshLEActionListForGamepads_ExpectAndReturn(ghBTRCoreHdl,eBTRMgrSuccess);
-
+    
     BTRMGR_Result_t result = BTRMGR_Init();
     TEST_ASSERT_EQUAL(BTRMGR_RESULT_SUCCESS, result);
 }
@@ -6579,7 +6578,7 @@ void test_BTRMGR_Init_Success_Complete(void){
     BTRCore_GetListOfPairedDevices_StubWithCallback(_mock_BTRCore_GetListOfPairedDevices_Success);
     BTRMgr_PI_Init_IgnoreAndReturn(eBTRMgrFailure);
     BTRMgr_SD_Init_IgnoreAndReturn(eBTRMgrFailure);
-
+    BTRCore_refreshLEActionListForGamepads_ExpectAnyArgs();
     BTRMGR_Result_t result = BTRMGR_Init();
     TEST_ASSERT_EQUAL(BTRMGR_RESULT_SUCCESS, result);
     free(ghBTRCoreHdl);
