0321390b9866d2b7e5719ed8bf4cf9485081add8
ANANTHMARIMUTHU <49443026+ANANTHMARIMUTHU@users.noreply.github.com>
2025-08-13 08:25:36 -0400

    Update test_btrMgr.c

--- unitTest/unitTest_btmgr/test_btrMgr.c
+++ unitTest/unitTest_btmgr/test_btrMgr.c
@@ -7517,7 +7517,7 @@ void test_BTRMGR_StartAudioStreamingOut_StartUp_FailureToGetAllProfiles(void) {
     // Call the function under test
     BTRMGR_Result_t result = BTRMGR_StartAudioStreamingOut_StartUp(adapterIdx, devOpType);
     
-    //BTRMGR_SD_GetData_IgnoreAndReturn(eBTRMgrSuccess);
+    BTRMGR_SD_GetData_IgnoreAndReturn(eBTRMgrSuccess);
     // Verify the result
     TEST_ASSERT_EQUAL(BTRMGR_RESULT_GENERIC_FAILURE, result);
 }
