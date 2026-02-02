7ac19832e973cb4ad8d8a2f5ca5a072899da5901
nmuthu523 <Natraj_Muthusamy@comcast.com>
2026-02-02 08:34:16 +0000

    RDKEMW-13205 : Airpods getting auto-connected even auto-connect is OFF.
    Reason for change:
    Disconnect the airpods when auto-connect is set to OFF for an audio
    device since the ACL connection remains active even after the
    authorization rejection.
    
    Priority: P1
    Test Procedure: Follow the steps provided in description.
    
    Risks: Medium
    Signed-off-by:Natraj Muthusamy <Natraj_Muthusamy@comcast.com>

--- src/ifce/btrMgr.c
+++ src/ifce/btrMgr.c
@@ -10457,6 +10457,10 @@ btrMgr_ConnectionInAuthenticationCb (
                 BTRMGRLOG_INFO ("Incoming Connection accepted for Audio Out device based on the response from UI\n");
             } else {
                 BTRMGRLOG_INFO ("Incoming Connection rejected for Audio Out device based on the response from UI\n");
+                if (strstr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") || (apstConnCbInfo->stKnownDevice.ui32VendorId == 834)) {
+                    btrMgr_ClearDisconnDevHoldOffTimer();
+                    btrMgr_SetDisconnDevHoldOffTimer(apstConnCbInfo->stKnownDevice.tDeviceId);
+                }
             }
             gEventRespReceived = 0;
         }
