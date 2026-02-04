5868b9b5d59bb92af8906ef0382dcff41d239669
natrajmuthusamy <52849136+natrajmuthusamy@users.noreply.github.com>
2026-02-04 16:31:42 +0530

    Update btrMgr.c

--- src/ifce/btrMgr.c
+++ src/ifce/btrMgr.c
@@ -10460,7 +10460,7 @@ btrMgr_ConnectionInAuthenticationCb (
                 if (strstr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") ||
                     (apstConnCbInfo->stKnownDevice.ui32VendorId == 834) ||
                     (apstConnCbInfo->stKnownDevice.ui32VendorId == 76)) {
-                    BTRCORELOG_INFO("Device remains connected even after authorization rejection; initiating a 5‑second timer to disconnect the AirPods.\n");
+                    BTRMGRELOG_INFO("Device remains connected even after authorization rejection; initiating a 5‑second timer to disconnect the AirPods.\n");
                     btrMgr_ClearDisconnDevHoldOffTimer();
                     btrMgr_SetDisconnDevHoldOffTimer(apstConnCbInfo->stKnownDevice.tDeviceId);
                 }
