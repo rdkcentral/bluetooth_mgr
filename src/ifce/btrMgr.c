0f38ff8e9fecdef4c8853b1ce6219899cc38c1f3
natrajmuthusamy <52849136+natrajmuthusamy@users.noreply.github.com>
2026-02-05 15:07:27 +0530

    Update btrMgr.c

--- src/ifce/btrMgr.c
+++ src/ifce/btrMgr.c
@@ -6183,7 +6183,7 @@ BTRMGR_StartAudioStreamingOut_StartUp (
                                     BTRMGRLOG_INFO ("Incoming Connection accepted for Audio Out device based on the response from UI\n");
                                 } else {
                                     BTRMGRLOG_INFO ("Incoming Connection rejected for Audio Out device based on the response from UI\n");
-                                    if (strstr(stDeviceInfo.pcDeviceName, "AirPods") ||
+                                    if (strcasestr(stDeviceInfo.pcDeviceName, "AirPods") ||
                                                (stDeviceInfo.ui32ModaliasVendorId == 834) ||
                                                 (stDeviceInfo.ui32ModaliasVendorId == 76)) {
                                          BTRMGRLOG_INFO("Device remains connected even after authorization rejection; initiating a 5‑second timer to disconnect the AirPods.\n");
@@ -10464,7 +10464,7 @@ btrMgr_ConnectionInAuthenticationCb (
                 BTRMGRLOG_INFO ("Incoming Connection accepted for Audio Out device based on the response from UI\n");
             } else {
                 BTRMGRLOG_INFO (">>>> Incoming Connection rejected for Audio Out device based on the response from UI <<<<<\n");
-                if (strstr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") ||
+                if (strcasestr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") ||
                     (apstConnCbInfo->stKnownDevice.ui32VendorId == 834) ||
                     (apstConnCbInfo->stKnownDevice.ui32VendorId == 76)) {
                     BTRMGRLOG_INFO("Device remains connected even after authorization rejection; initiating a 5‑second timer to disconnect the AirPods.\n");
@@ -10481,7 +10481,7 @@ btrMgr_ConnectionInAuthenticationCb (
         }
         else {
             BTRMGRLOG_ERROR ("Incoming Connection denied\n");
-            if (strstr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") || (apstConnCbInfo->stKnownDevice.ui32VendorId == 834)) {
+            if (strcasestr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") || (apstConnCbInfo->stKnownDevice.ui32VendorId == 834)) {
                 btrMgr_ClearDisconnDevHoldOffTimer();
                 btrMgr_SetDisconnDevHoldOffTimer(apstConnCbInfo->stKnownDevice.tDeviceId);
             }
