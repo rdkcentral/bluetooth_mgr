c3f534b2bd7398a55849bdde82c766ac5a4d6cd2
natrajmuthusamy <52849136+natrajmuthusamy@users.noreply.github.com>
2026-02-12 16:01:45 +0530

    Update btrMgr.c

--- src/ifce/btrMgr.c
+++ src/ifce/btrMgr.c
@@ -6184,8 +6184,8 @@ BTRMGR_StartAudioStreamingOut_StartUp (
                                 } else {
                                     BTRMGRLOG_INFO ("Incoming Connection rejected for Audio Out device based on the response from UI\n");
                                     if (strstr(stDeviceInfo.pcDeviceName, "AirPods") ||
-                                        (stDeviceInfo.ui32ModaliasVendorId == 834) ||
-                                        (stDeviceInfo.ui32ModaliasVendorId == 76)) {
+                                        (stDeviceInfo.ui32ModaliasVendorId == BTRMGR_APPLE_VENDOR_ID_1) ||
+                                        (stDeviceInfo.ui32ModaliasVendorId == BTRMGR_APPLE_VENDOR_ID_2)) {
                                          BTRMGRLOG_INFO("Device remains connected even after authorization rejection; initiating a 5‑second timer to disconnect the AirPods.\n");
                                          btrMgr_ClearDisconnDevHoldOffTimer();
                                          btrMgr_SetDisconnDevHoldOffTimer(stDeviceInfo.tDeviceId);
@@ -9091,14 +9091,14 @@ btrmgr_DisconnectDeviceTimerCb (
     gAuthDisconnDevTimeOutRef = 0; // TODO: Is this really required ? Should we call btrMgr_ClearDisconnDevHoldOffTimer to perform g_source_destroy
                                    // Can we even call a g_source_destroy from the context of the timer Cb associated with the same Id/Ref ?
 
-	if (ghBTRMgrDevHdlCurStreaming != aBTRCoreDevId) {
+    if (ghBTRMgrDevHdlCurStreaming != aBTRCoreDevId) {
         if (BTRCore_DisconnectDevice (ghBTRCoreHdl, aBTRCoreDevId, lenBTRCoreDeviceType) != enBTRCoreSuccess) {
             BTRMGRLOG_ERROR ("Post Device disconnect Cb timeout Failed!\n");
         }
 	 } else {
         BTRMGRLOG_INFO("Not disconnecting device, as device has started streaming\n");
-	 }
-    return FALSE;
+     }
+     return FALSE;
 }
 
 static gboolean
@@ -10463,8 +10463,8 @@ btrMgr_ConnectionInAuthenticationCb (
             } else {
                 BTRMGRLOG_INFO ("Incoming Connection rejected for Audio Out device based on the response from UI\n");
                 if (strstr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") ||
-                    (apstConnCbInfo->stKnownDevice.ui32VendorId == 834) ||
-                    (apstConnCbInfo->stKnownDevice.ui32VendorId == 76)) {
+                    (apstConnCbInfo->stKnownDevice.ui32VendorId == BTRMGR_APPLE_VENDOR_ID_1) ||
+                    (apstConnCbInfo->stKnownDevice.ui32VendorId == BTRMGR_APPLE_VENDOR_ID_2)) {
                       BTRMGRLOG_INFO("Device remains connected even after authorization rejection; initiating a 5-second timer to disconnect the AirPods.\n");
                       btrMgr_ClearDisconnDevHoldOffTimer();
                       btrMgr_SetDisconnDevHoldOffTimer(apstConnCbInfo->stKnownDevice.tDeviceId);
@@ -10479,7 +10479,7 @@ btrMgr_ConnectionInAuthenticationCb (
         }
         else {
             BTRMGRLOG_ERROR ("Incoming Connection denied\n");
-            if (strstr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") || (apstConnCbInfo->stKnownDevice.ui32VendorId == 834)) {
+            if (strstr(apstConnCbInfo->stKnownDevice.pcDeviceName, "AirPods") || (apstConnCbInfo->stKnownDevice.ui32VendorId == BTRMGR_APPLE_VENDOR_ID_1)) {
                 btrMgr_ClearDisconnDevHoldOffTimer();
                 btrMgr_SetDisconnDevHoldOffTimer(apstConnCbInfo->stKnownDevice.tDeviceId);
             }
