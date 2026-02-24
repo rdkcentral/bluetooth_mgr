5dfe96ac54b20a93d4827dc48a58774e9c53c1ff
tharun-kumarv <105273644+tharun-kumarv@users.noreply.github.com>
2026-02-24 14:53:06 +0530

    XIONE-18219: Xbox Gen3 controller not discoverable in UI
    Reason for change: Set Gen3 controller with a temporary name during pairing when BlueZ reports MAC as device name
    Test Procedure: Check ticket description
    
    Risks: Medium
    Priority: P1

--- src/ifce/btrMgr.c
+++ src/ifce/btrMgr.c
@@ -1528,8 +1528,20 @@ btrMgr_GetDiscoveredDevInfo (
 
     for (j = 0; j < gListOfDiscoveredDevices.m_numOfDevices; j++) {
         if (ahBTRMgrDevHdl == gListOfDiscoveredDevices.m_deviceProperty[j].m_deviceHandle) {
+        if (!btrMgr_IsDevNameSameAsAddress(
+                gListOfDiscoveredDevices.m_deviceProperty[j].m_name,
+                gListOfDiscoveredDevices.m_deviceProperty[j].m_deviceAddress,
+                strlen(gListOfDiscoveredDevices.m_deviceProperty[j].m_deviceAddress)
+            ))
+        {
             MEMCPY_S(apBtMgrDiscDevInfo,sizeof(BTRMGR_DiscoveredDevices_t), &gListOfDiscoveredDevices.m_deviceProperty[j], sizeof(BTRMGR_DiscoveredDevices_t));
         }
+			else {
+            BTRMGRLOG_WARN("Skipping discovered details and UI update for device %s (%s): name matches address\n",
+                gListOfDiscoveredDevices.m_deviceProperty[j].m_name,
+                gListOfDiscoveredDevices.m_deviceProperty[j].m_deviceAddress);
+        }
+		}
     }
 }
 
@@ -10131,8 +10143,17 @@ btrMgr_DeviceDiscoveryCb (
 
                 btrMgr_MapDevstatusInfoToEventInfo ((void*)&astBTRCoreDiscoveryCbInfo->device, &lstEventMessage, BTRMGR_EVENT_DEVICE_DISCOVERY_UPDATE);
 
-                if (gfpcBBTRMgrEventOut) {
+				if (!btrMgr_IsDevNameSameAsAddress(
+                    lstEventMessage.m_discoveredDevice.m_name,
+                    lstEventMessage.m_discoveredDevice.m_deviceAddress,
+                    strlen(lstEventMessage.m_discoveredDevice.m_deviceAddress)))
+                    {
+                    if (gfpcBBTRMgrEventOut) {
                     gfpcBBTRMgrEventOut(lstEventMessage); /*  Post a callback */
+					}
+                }
+				else {
+                     BTRMGRLOG_INFO("Skipping device update: name is same as address\n");
                 }
             }
         }
