a57a12790a57fea0d39e15bd9db030d154719dc7
ppalan289 <preethi_palanisamy@comcast.com>
2026-09-07 14:36:46 +0000

    RDKEMW-20382: bt-mgr changes
    Reason for change: Crash fix
    Test Procedure: sanityTesting
    Risks: Low
    Priority: P2
    
    Signed-off-by: ppalan289 <preethi_palanisamy@comcast.com>

--- src/ifce/btrMgr.c
+++ src/ifce/btrMgr.c
@@ -396,6 +396,7 @@ static inline void btrMgr_ClearCheckFirmwareVersionHoldOffTimer(void);
 static inline void btrMgr_ClearLastPairedDeviceStatusHoldOffTimer (void);
 #endif
 
+/* Preethi comment-1 */
 //static eBTRMgrRet btrMgr_SetDiscoveryFilter (BTRMGR_DiscoveryHandle_t* ahdiscoveryHdl, BTRMGR_ScanFilter_t aeScanFilterType, void* aFilterValue);
 //static eBTRMgrRet btrMgr_ClearDiscoveryFilter (BTRMGR_DiscoveryHandle_t*   ahdiscoveryHdl);
 
